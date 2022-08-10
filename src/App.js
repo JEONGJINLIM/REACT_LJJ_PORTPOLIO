3d43a2024048a0c9a2e7435ead21a4238673f5fb
limjj88 <limjj88@naver.com>
2022-08-10 09:00:30 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { Route } from 'react-router-dom';
+
+//common
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+
+//main
+import Visual from './components/main/Visual';
+import Content from './components/main/Content';
+
+//sub
+import Department from './components/sub/Department';
+import Community from './components/sub/Community';
+import Gallery from './components/sub/Gallery';
+import Youtube from './components/sub/Youtube';
+import Location from './components/sub/Location';
+import Members from './components/sub/Members';
+
+import './scss/style.scss';
+
+function App() {
+	return (
+		<>
+			<Header />
+
+			<Route exact path='/'>
+				<Visual />
+				<Content />
+			</Route>
+
+			<Route path='/department' component={Department} />
+			<Route path='/community' component={Community} />
+			<Route path='/gallery' component={Gallery} />
+			<Route path='/youtube' component={Youtube} />
+			<Route path='/location' component={Location} />
+			<Route path='/members' component={Members} />
+
+			<Footer />
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/common/Header.js
@@ -0,0 +1,54 @@
+import { Link, NavLink } from 'react-router-dom';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+function Header() {
+	const active = { color: '#403730' };
+
+	return (
+		<header>
+			<h1>
+				<Link to='/'>LOGO</Link>
+			</h1>
+			<nav>
+				<ul id='gnb'>
+					<li>
+						<NavLink to='/department' activeStyle={active}>
+							DEPARTMENT
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/community' activeStyle={active}>
+							COMMUNITY
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/gallery' activeStyle={active}>
+							GALLERY
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/youtube' activeStyle={active}>
+							YOUTUBE
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/location' activeStyle={active}>
+							LOCATION
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/members' activeStyle={active}>
+							MEMBERS
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+}
+
+export default Header;
